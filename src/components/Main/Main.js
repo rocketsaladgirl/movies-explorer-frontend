65735a0dfb1c046de8a45329892f1b2fbaaf9d04
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-06-27 18:07:58 +0300

    добавлен блок AboutProject

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Promo from '../Promo/Promo';
-// import AboutProject from '../AboutProject/AboutProject';
+import AboutProject from '../AboutProject/AboutProject';
 // import Techs from '../Techs/Techs';
 // import AboutMe from '../AboutMe/AboutMe';
 // import Portfolio from '../Portfolio/Portfolio';
@@ -12,8 +12,8 @@ function Main() {
     return (
         <main className='main'>
             <Promo />
-            {/* <AboutProject />
-            <Techs />
+            <AboutProject />
+            {/*<Techs />
             <AboutMe />
             <Portfolio /> */}
         </main>
