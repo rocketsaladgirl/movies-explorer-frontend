484c2149b98236dc512a1faf642d2a96199ec06f
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-07-15 15:36:04 +0300

    Скорректирован App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import './App.css';
+
+import { mainApi } from '../../utils/MainApi';
+import { CurrentUserContextProvider } from '../../context/CurrentUserContext';
+import { CurrentMoviesContextProvider } from '../../context/CurrentMoviesContext';
 
 import Header from '../Header/Header';
 import Main from '../Main/Main';
@@ -17,69 +21,125 @@ import SavedMovies from '../SavedMovies/SavedMovies';
 
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
-import moviesBase from '../../utils/moviesBase';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
+
+import Preloader from '../Preloader/Preloader';
+import Popup from '../Popup/Popup';
+import PopupContent from '../PopupContent/PopupContent';
+
 
 function App() {
-  const [moviesList, setMoviesList] = React.useState([]);
-  const [addedMoviesList, setAddedMoviesList] = React.useState([]);
+  const [isLogin, setIsLogin] = useState(false);
+  const [currentUser, setCurrentUser] = useState({name: '', email: ''});
+  const [isTokenCheck, setIsTokenCheck] = useState(false);
+  const [savedMovies, setSavedMovies] = useState([]);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [text, setText] = useState('');
 
-  React.useEffect(() => {
-    setMoviesList(moviesBase);
+  function handlePopupClose() {
+    setIsPopupOpen(false);
+    setText('');
+  }
+
+  useEffect(() => {
+    const userId = localStorage.getItem('currentId');
+
+    if (userId) {
+      mainApi.getUserInfo()
+        .then((userData) => {
+          setCurrentUser(userData);
+          setIsLogin(true);
+        })
+        .catch((err) => {
+          setIsPopupOpen(true);
+          setText(err);
+        })
+        .finally(() => {
+          setIsTokenCheck(true);
+        })
+    }
+    else {
+      setIsTokenCheck(true);
+      setIsLogin(false);
+    }
   }, []);
 
-  React.useEffect(() => {
-    setAddedMoviesList(moviesBase.filter((movie) => {
-      return movie.saved
-    }))
-  }, []);
 
   return (
     <div className='page'>
       <div className='page__container'>
-        <Routes>
-          <Route exact path='/' element={
-            <>
-              <Header loggedIn={false} />
-              <Main />
-              <Footer />
-            </>
-          }/>
-          <Route path='/signin' element={
-            <>
-              <Login />
-            </>  
-          }/>
-          <Route path='/signup' element={
-            <>
-              <Register />
-            </> 
-          }/>
-          <Route exact path='/movies' element={
-            <>
-              <Header loggedIn={true} />
-              <Movies moviesList={moviesList}/>
-              <Footer />
-            </>
-          }/>
-          <Route exact path='/saved-movies' element={
-            <>
-              <Header loggedIn={true} />
-              <SavedMovies moviesList={addedMoviesList}/>
-              <Footer />
-            </>
-          }/>
-          <Route exact path='/profile' element={
-            <>
-              <Header loggedIn={true} />
-              <Profile />
-            </>
-          }/>
-          <Route path='*' element={
-            <>
-              <NotFoundPage />
-            </>
-          }/>
-        </Routes>  
+        {isTokenCheck
+            ? <CurrentUserContextProvider context={{ currentUser, setCurrentUser }}>
+                <CurrentMoviesContextProvider context={{ savedMovies, setSavedMovies }}>
+                  <Routes>
+                    <Route exact path='/' element={
+                      <>
+                        <Header isLogin={isLogin} />
+                        <Main />
+                        <Footer />
+                      </>
+                    }/>
+                    <Route path='/signup' element={
+                      <ProtectedRoute isLogin={!isLogin}>
+                        <Register changeStatus={setIsLogin} />
+                      </ProtectedRoute>
+                    }/>
+
+                    <Route path='/signin' element={
+                      <ProtectedRoute isLogin={!isLogin}>
+                        <Login changeStatus={setIsLogin}/>
+                      </ProtectedRoute>
+                    }/>
+
+                    <Route path='/movies' element={
+                      <ProtectedRoute isLogin={isLogin}>
+                        <>
+                          <Header isLogin={isLogin} />
+                          <Movies />
+                          <Footer />
+                        </>
+                      </ProtectedRoute>
+                    }/> 
+
+                    <Route path='/saved-movies' element={
+                      <ProtectedRoute isLogin={isLogin}>
+                        <>
+                          <Header isLogin={isLogin} />
+                          <SavedMovies />
+                          <Footer />
+                        </>
+                      </ProtectedRoute>
+                    }/>
+
+                    <Route path='/profile' element={
+                      <ProtectedRoute isLogin={isLogin}>
+                        <>
+                          <Header isLogin={isLogin}/>
+                          <Profile changeStatus={setIsLogin} />
+                        </>
+                      </ProtectedRoute>
+                    }/>
+
+                    <Route path='*' element={<Navigate to='/404' replace />}/>
+
+                    <Route path='/404' element={
+                      <>
+                        <NotFoundPage />
+                      </>
+                    }/>
+
+                  </Routes> 
+
+                  <Popup isOpen={isPopupOpen}>
+                    <PopupContent onClose={handlePopupClose} text={text} />
+                  </Popup> 
+                </CurrentMoviesContextProvider>
+            </CurrentUserContextProvider>
+
+            : <div className='page__preloader'>
+                <Preloader />
+            </div> 
+        }
       </div>
     </div>
   );
