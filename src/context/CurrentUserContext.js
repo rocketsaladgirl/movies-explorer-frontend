42265a70af0b9c6be5615454b1f24a66d2678f86
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-07-15 12:02:18 +0300

    Добавлен отдельный context для фильмов и пользователя

--- src/context/CurrentUserContext.js
+++ src/context/CurrentUserContext.js
@@ -1,3 +1,18 @@
-import React from "react";
+import { createContext, useContext } from "react";
 
-export const CurrentUserContext = React.createContext();
+const initialUserData = {
+  name: '',
+  email: '',
+};
+
+const CurrentUserContext = createContext(initialUserData);
+
+export function useCurrentUserContext() {
+    const context = useContext(CurrentUserContext);
+    return context;
+};
+
+export function CurrentUserContextProvider({ children, ...props }) {
+  return <CurrentUserContext.Provider value={props.context}>{children}</CurrentUserContext.Provider>;
+};
+
