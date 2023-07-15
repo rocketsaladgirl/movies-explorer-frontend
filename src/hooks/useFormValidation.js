a68edf4e93f20be34cc46d6a17194f4c7de43214
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-07-15 11:46:52 +0300

    исправлен файл useFormValidation

--- src/hooks/useFormValidation.js
+++ src/hooks/useFormValidation.js
@@ -1,39 +1,28 @@
 import { useState, useCallback } from 'react';
 
-const useFormValidation = () => {
-  const [ values, setValues ] = useState({}),
-        [ errors, setErrors ] = useState({}),
-        [ isValid, setIsValid ] = useState(false),
-        [ currentInputName, setCurrentInputName ] = useState('');
+export const useFormValidation = (defaultValues = {}, defaultFormValidity = false) => {
+  const [errors, setErrors] = useState({});
+  const [values, setValues] = useState(defaultValues);
+  const [inputVilidities, setInputVilidities] = useState({});
+  const [isValid, setIsValid] = useState(defaultFormValidity);
 
-  const handleChange = ({ target }) => {
-    const { name, value } = target;
-
-    setValues({ ...values, [name]: value });
-    setErrors({ ...errors, [name]: target.validationMessage });
-    setIsValid(target.closest('form').checkValidity());
-    setCurrentInputName(name);
+  const handleChange = ({target}) => {
+    const { name, value, validationMessage, validity, form } = target;
+    setValues({...values, [name]: value});
+    setErrors({...errors, [name]: validationMessage});
+    setInputVilidities({...inputVilidities, [name]: validity.valid});
+    setIsValid(form.checkValidity());
   };
 
   const resetForm = useCallback(
-    (newValues = {}, newErrors = {}, newIsValid = false) => {
+    (newIsValid = false, newValues = {}, newErrors = {}, newInputVilidities = {}) => {
       setValues(newValues);
       setErrors(newErrors);
+      setInputVilidities(newInputVilidities);
       setIsValid(newIsValid);
     },
-    [setValues, setErrors, setIsValid]
+    [setValues, setErrors, setIsValid, setInputVilidities]
   );
 
-  return {
-    values,
-    setValues,
-    errors,
-    isValid,
-    setIsValid,
-    currentInputName,
-    handleChange,
-    resetForm,
-  };
+  return { values, handleChange, errors, isValid, resetForm, inputVilidities };
 };
-
-export default useFormValidation;
